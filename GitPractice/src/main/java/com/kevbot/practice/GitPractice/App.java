87c5a4e02c5d12b9eb8ee86e6587dde5d69a169e
Kevin Key <kkey8@vols.utk.edu>
2019-06-23 13:51:05 -0500

    Goodbye again world

--- GitPractice/src/main/java/com/kevbot/practice/GitPractice/App.java
+++ GitPractice/src/main/java/com/kevbot/practice/GitPractice/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Goodbye World!" );
+        System.out.println( "Goodbye Again World!" );
     }
 }
