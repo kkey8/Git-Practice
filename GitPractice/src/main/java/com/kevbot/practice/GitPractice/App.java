c4d31d034ded3db7a49b2638c1c4c15c57c92b75
Kevin Key <kkey8@vols.utk.edu>
2019-06-23 21:20:34 -0500

    BufferedWriter commit

--- GitPractice/src/main/java/com/kevbot/practice/GitPractice/App.java
+++ GitPractice/src/main/java/com/kevbot/practice/GitPractice/App.java
@@ -1,13 +1,28 @@
 package com.kevbot.practice.GitPractice;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+    	File f = new File("howdy.txt");
+    	try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+			bw.write("Hello world");
+			bw.newLine();
+			bw.write("I think this worked!");
+			bw.flush();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
         System.out.println( "Hello World!" );
         System.out.println(2+5);
     }
